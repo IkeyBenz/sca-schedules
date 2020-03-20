defb64fa36df4baba912854aba87caf9a64ad1a4
IkeyBenz <ikey.benz@gmail.com>
2020-03-20 05:53:38 -0400

    cleanup

--- src/factory/AdminScreenCreator.tsx
+++ src/factory/AdminScreenCreator.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import firebase from 'firebase';
+import React, { useEffect, useState } from 'react';
 
 import { AdminScreen } from '../ui';
 import { scheduleManager } from '../service';
