0a41425727b78b378321c96cea2f19aac1912a92
IkeyBenz <ikey.benz@gmail.com>
2020-03-20 07:48:05 -0400

    Updates

---
+++ precache-manifest.ab2a87a54621b69330c834a36ce020c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524324b27b191a24f6c99dc75ddf4e91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "864936f7f1ed21723855",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "bd83ff6a421e7ab085d7",
+    "url": "/static/css/main.f643cfd7.chunk.css"
+  },
+  {
+    "revision": "864936f7f1ed21723855",
+    "url": "/static/js/2.b03d9627.chunk.js"
+  },
+  {
+    "revision": "ea9618f8fb2c49822fb991826dc462bc",
+    "url": "/static/js/2.b03d9627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd83ff6a421e7ab085d7",
+    "url": "/static/js/main.22548009.chunk.js"
+  },
+  {
+    "revision": "42b45c8fc6b07ea2ab24",
+    "url": "/static/js/runtime-main.aaa53ec6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5dbbb1edac105d9dfc89d3c1dfe5fed1.js"
+  "/precache-manifest.ab2a87a54621b69330c834a36ce020c5.js"
 );
 
 self.addEventListener('message', (event) => {
