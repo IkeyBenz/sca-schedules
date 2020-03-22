a85ee22f414abfeec652067f5d8287d94515f116
IkeyBenz <ikey.benz@gmail.com>
2020-03-21 21:38:03 -0400

    styled the schedule tables

--- src/ui/components/ScheduleCard.tsx
+++ src/ui/components/ScheduleCard.tsx
@@ -10,23 +10,25 @@ const ScheduleCard: React.FC<ScheduleCardProps> = ({ schedule }) => {
   const { title, rows } = schedule;
 
   return (
-    <div>
+    <div className="schedule-card">
       <h1>{title}</h1>
-      <table className="table">
-        <thead>
+      <table className="table table-striped table-bordered table-hover shadow">
+        <thead className="text-light">
           <tr>
             {rows[0].map(col => (
               <th>{col}</th>
             ))}
           </tr>
         </thead>
-        {rows.slice(1).map(row => (
-          <tr>
-            {row.map(col => (
-              <td>{col}</td>
-            ))}
-          </tr>
-        ))}
+        <tbody>
+          {rows.slice(1).map(row => (
+            <tr>
+              {row.map(col => (
+                <td>{col}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
