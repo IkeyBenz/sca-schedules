da0843d3be7e30211027098df09d498bb953faeb
Henry <henrybarnathan@me.com>
2020-03-24 16:30:17 -0400

    Updates

---
+++ precache-manifest.1ee5b8122d9ed3414cd340d37d3f7f6c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fc5e4a22f0210a22d18f7b003b6663",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88319f014e5599fdae0e",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "a7569ea087a4f9ef0b65",
+    "url": "/static/css/main.2a14492d.chunk.css"
+  },
+  {
+    "revision": "88319f014e5599fdae0e",
+    "url": "/static/js/2.ba3b2584.chunk.js"
+  },
+  {
+    "revision": "ea9618f8fb2c49822fb991826dc462bc",
+    "url": "/static/js/2.ba3b2584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7569ea087a4f9ef0b65",
+    "url": "/static/js/main.ce785ee0.chunk.js"
+  },
+  {
+    "revision": "42b45c8fc6b07ea2ab24",
+    "url": "/static/js/runtime-main.aaa53ec6.js"
+  },
+  {
+    "revision": "b2d06419c5987331147b9538bb1c5559",
+    "url": "/static/media/SCALogo.b2d06419.png"
+  },
+  {
+    "revision": "3acec54637b48e318403c2acea476995",
+    "url": "/static/media/spinner.3acec546.gif"
+  }
+]);
