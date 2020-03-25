b62c3b14f0d7053e7ad398ba5cd447517b0b8a29
IkeyBenz <ikey.benz@gmail.com>
2020-03-25 02:15:27 -0400

    added logo to index page

--- src/ui/screens/Index.tsx
+++ src/ui/screens/Index.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
-import { database } from '../../service/index';
+import SCALogo from '../assets/SCALogo.png';
 
 const IndexScreen: React.FC = () => {
   return (
-    <div className="w-100 d-flex align-items-center justify-content-center">
-      <nav className="d-flex flex-column align-items-center my-3 site-nav-index">
-        <a href="/#/classes" className="btn btn-primary btn-lg my-3">
-          Schedule of Remote Classes
-        </a>
-        <a href="/#/minyanim" className="btn btn-primary btn-lg my-3">
-          Schedule of Remote Minyanim
-        </a>
-        <a href="/#/letters" className="btn btn-primary btn-lg my-3">
-          Letters to the Community
-        </a>
-        <a href="/#/tehillim" className="btn btn-primary btn-lg my-3">
-          Request Tehillim for Refuah
-        </a>
-        <a href="mailto:info@scaupdates.org" className="btn btn-primary btn-lg my-3">
-          Contact the SCA
-        </a>
-      </nav>
-    </div>
+    <>
+      <div className="container">
+        <img
+          src={SCALogo}
+          className="centered-image my-3"
+          height="150"
+          alt=""
+        />
+      </div>
+      <div className="w-100 d-flex align-items-center justify-content-center">
+        <nav className="d-flex flex-column align-items-center my-3 site-nav-index">
+          <a href="/#/classes" className="btn btn-primary btn-lg my-3">
+            Schedule of Remote Classes
+          </a>
+          <a href="/#/minyanim" className="btn btn-primary btn-lg my-3">
+            Schedule of Remote Minyanim
+          </a>
+          <a href="/#/letters" className="btn btn-primary btn-lg my-3">
+            Letters to the Community
+          </a>
+          <a href="/#/tehillim" className="btn btn-primary btn-lg my-3">
+            Request Tehillim for Refuah
+          </a>
+          <a
+            href="mailto:info@scaupdates.org"
+            className="btn btn-primary btn-lg my-3">
+            Contact the SCA
+          </a>
+        </nav>
+      </div>
+    </>
   );
 };
 
