3ff1ea6a22daf749a9eb7996a1857820507f3f63
IkeyBenz <ikey.benz@gmail.com>
2020-03-25 02:16:02 -0400

    Updates

---
+++ precache-manifest.1cc558952195bb37adb3a1ec1d2d78a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd5e655eabcbecc0962a13dc4523bc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f065f1be52eb9e73df37",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "bd4a099e359d3a8a7a6b",
+    "url": "/static/css/main.4f9a39a5.chunk.css"
+  },
+  {
+    "revision": "f065f1be52eb9e73df37",
+    "url": "/static/js/2.b8b85f1a.chunk.js"
+  },
+  {
+    "revision": "ea9618f8fb2c49822fb991826dc462bc",
+    "url": "/static/js/2.b8b85f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4a099e359d3a8a7a6b",
+    "url": "/static/js/main.da47beb0.chunk.js"
+  },
+  {
+    "revision": "42b45c8fc6b07ea2ab24",
+    "url": "/static/js/runtime-main.aaa53ec6.js"
+  },
+  {
+    "revision": "b2d06419c5987331147b9538bb1c5559",
+    "url": "/static/media/SCALogo.b2d06419.png"
+  },
+  {
+    "revision": "3acec54637b48e318403c2acea476995",
+    "url": "/static/media/spinner.3acec546.gif"
+  }
+]);
