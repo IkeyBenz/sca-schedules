f7cf43229bad91454f1b58ca84e62badc404b8be
IkeyBenz <ikey.benz@gmail.com>
2020-04-13 13:46:03 -0400

    [refactor] ScheduleService to output a DataFrame (from the google sheet) instead of an array of schedules

--- src/service/ScheduleService.ts
+++ src/service/ScheduleService.ts
@@ -1,34 +1,19 @@
-import { Schedule, Database } from '../types';
+import { Database, DataFrame } from '../types';
 
 class ScheduleService {
   private storage: Database;
+  static scheduleRef = '1TS3fZHRPlhI4L_URhKJXGa48eOy6BiqM1GeSUJL9jFs/live-website-feed';
 
   constructor(database: Database) {
     this.storage = database;
   }
 
-  async addSchedule(schedule: Schedule) {
-    return this.storage.push('schedules', schedule);
+  async getAllSchedules(): Promise<DataFrame> {
+    return await this.storage.read(ScheduleService.scheduleRef);
   }
 
-  async removeSchedule(_id: firebase.database.Reference) {
-    return this.storage.delete('schedules', _id);
-  }
-
-  async updateSchedule(_id: firebase.database.Reference, newSchedule: Schedule) {
-    return this.storage.write(`schedules/${_id}`, newSchedule);
-  }
-
-  async getAllSchedules(): Promise<Schedule[]> {
-    return this._alphabetize(await this.storage.findAll('schedules'));
-  }
-
-  onSchedulesChanged(cb: (schedules: Schedule[]) => void) {
-    this.storage.onChange('schedules', data => cb(this._alphabetize(data)));
-  }
-
-  _alphabetize(schedules: Schedule[]) {
-    return schedules ? schedules.sort((a, b) => a.title > b.title ? 1 : a.title < b.title ? -1 : 0) : [];
+  onSchedulesChanged(cb: (data: DataFrame) => void) {
+    this.storage.onChange(ScheduleService.scheduleRef, cb);
   }
 }
 
